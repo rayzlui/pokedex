ef180b372f10ba0c81044526b75ccfcbb303f70e
Ray <rayzlui@gmail.com>
2020-01-20 19:33:34 -0800

    update reducer to differentiate pokedex data vs specific data

--- src/reducers/fetchDataReducer.js
+++ src/reducers/fetchDataReducer.js
@@ -7,27 +7,47 @@ import {
 const initialState = {
   isFetching: false,
   data: null,
+  listData: null,
   error: null,
 };
-
+const pokedexi = {
+  kanto: true,
+  'original-johto': true,
+  hoenn: true,
+  'original-sinnoh': true,
+  'original-unova': true,
+  'kalos-central': true,
+  'kalos-coastal': true,
+  'kalos-mountain': true,
+};
 export function fetchDataReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_DATA_ERROR:
       return Object.assign({}, state, {
         isFetching: false,
         data: null,
+        listData: null,
         error: action.error,
       });
     case FETCH_DATA_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        data: action.data,
-        error: null,
-      });
+      if (pokedexi[action.data.name]) {
+        return Object.assign({}, state, {
+          isFetching: false,
+          listData: action.data,
+          data: state.data,
+          error: null,
+        });
+      } else {
+        return Object.assign({}, state, {
+          isFetching: false,
+          listData: state.listData,
+          data: action.data,
+          error: null,
+        });
+      }
     case FETCH_DATA_START:
       return Object.assign({}, state, {
         isFetching: true,
-        data: null,
         error: null,
       });
     default:

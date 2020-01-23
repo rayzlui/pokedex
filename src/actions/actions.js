fe2dc159a6fb0d2882b2b9b3f41abcfeed99e362
Ray <rayzlui@gmail.com>
2020-01-22 22:17:28 -0800

    update actions to handle pokedex request

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -4,21 +4,38 @@ import {
   FETCH_DATA_SUCCESS,
   CHANGE_POKEDEX,
   CHANGE_DISPLAY_TO,
+  FETCH_POKEDEX_START,
+  FETCH_POKEDEX_ERROR,
+  FETCH_POKEDEX_SUCESS,
 } from './actionTypes';
 
-export function fetchData(info, display, type) {
-  type = type === undefined ? display : type;
+export function fetchData(info, type) {
   return async function requestPokemon(dispatch) {
     dispatch(fetchDataStart());
     const response = await fetch(`https://pokeapi.co/api/v2/${type}/${info}`);
     if (response.status === 200) {
       const data = await response.json();
-      dispatch(changeDisplay(display));
+      dispatch(changeDisplay(type));
       dispatch(fetchDataSuccess(data));
     } else {
       const { status, statusText, url } = response;
       const errorInfo = { status, statusText, url };
       dispatch(fetchDataError(errorInfo));
+    }
+  };
+}
+
+export function fetchPokedex(region) {
+  return async function requestPokedex(dispatch) {
+    dispatch(fetchPokedexStart());
+    const response = await fetch(`https://pokeapi.co/api/v2/pokedex/${region}`);
+    if (response.status === 200) {
+      const data = await response.json();
+      dispatch(fetchPokedexSuccess(data));
+    } else {
+      const { status, statusText, url } = response;
+      const errorInfo = { status, statusText, url };
+      dispatch(fetchPokedexError(errorInfo));
     }
   };
 }
@@ -42,12 +59,24 @@ export function fetchDataSuccess(data) {
   return { type: FETCH_DATA_SUCCESS, data: data };
 }
 
+export function fetchPokedexSuccess(data) {
+  return { type: FETCH_POKEDEX_SUCESS, data: data };
+}
+
 export function fetchDataError(error) {
   return { type: FETCH_DATA_ERROR, error: error };
 }
 
+export function fetchPokedexError(error) {
+  return { type: FETCH_POKEDEX_ERROR, error: error };
+}
+
 export function fetchDataStart() {
   return { type: FETCH_DATA_START };
+}
+
+export function fetchPokedexStart() {
+  return { type: FETCH_POKEDEX_START };
 }
 
 export function changePokedex(region) {

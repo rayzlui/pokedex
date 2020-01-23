44b6c115d628726409167edd002747a7ea3faff2
Ray <rayzlui@gmail.com>
2020-01-22 22:19:39 -0800

    update root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { displaying } from './changeDisplay';
 import { pokedex } from './changePokedexReducers';
 import { fetchDataReducer } from './fetchDataReducer';
+import { fetchPokedexReducer } from './fetchPokedexReducer';
 
 export const rootReducer = combineReducers({
   requestPokemon: fetchDataReducer,
+  requestPokedex: fetchPokedexReducer,
   pokedex,
   displaying,
 });
